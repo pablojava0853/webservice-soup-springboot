81f278ae1ef9bb8e40501fc57cd25ea9c12278a0
Pablo Gonzalez <pablojava853@outlook.com>
2021-07-12 22:17:10 -0300

    Commit inicial

---
+++ src/main/java/com/pgs/soap/webservices/custumersadministration/CustumersAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.pgs.soap.webservices.custumersadministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustumersAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustumersAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgs/soap/webservices/custumersadministration/CustumersAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgs.soap.webservices.custumersadministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustumersAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
